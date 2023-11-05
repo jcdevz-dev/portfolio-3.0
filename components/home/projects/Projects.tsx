d274db5b86e33a90626d06d53b6fdc643d3003c9
jc <dev.johncarlo.devera@gmail.com>
2023-11-05 08:41:30 +0800

    fix typos and updated links of projects

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -22,13 +22,14 @@ const projects = [
     imgSrc: "/project-imgs/elancerz.png",
     code: "https://www.github.com",
     projectLink: "https://elancerz.com/",
-    tech: ["React", "Nextjs", "Sytled Components","Firebase Auth", "Laravel"],
+    tech: ["React", "Nextjs", "Sytled Components", "Firebase Auth", "Laravel"],
     description:
       "A Talent finder app for anything around the world. This is my first time as a React developer job.",
     modalContent: (
       <>
         <p>
-         Worked as a senior developer and created dynamic reusable components, integrated calendly, messaging and project management features.
+          Worked as a senior developer and created dynamic reusable components,
+          integrated calendly, messaging and project management features.
         </p>
         <p>
           The tech stack is based on Nextjs with the custom Styled components,
@@ -41,17 +42,20 @@ const projects = [
     title: "Agency Iron Admin",
     imgSrc: "/project-imgs/agency-iron-crm.png",
     code: "https://www.github.com",
-    projectLink: "https://agency-iron-crm.herokuapp.com/",
+    // projectLink: "https://agency-iron-crm.herokuapp.com/",
+    projectLink: "#",
     tech: ["React", "MUI", "MongoDB", "Postgres", "Python"],
     description:
-      "A Dashboard for clients and admin of Rocky Mountain West Insurance LLC. using Vuejs and re-worked on Reactjs.",
+      "A Dashboard for clients and admin of Rocky Mountain West Insurance LLC. using Vuejs and re-worked using Reactjs.",
     modalContent: (
       <>
         <p>
-          Worked as a senior developer for this web application. Integrated api&apos;s, Created dynamic and reusable components.
+          Worked as a senior developer for this web application. Integrated
+          api&apos;s, Created dynamic and reusable components.
         </p>
         <p>
-          I work primarily on the frontend, creating reusable components and inegrating api.
+          I work primarily on the frontend, creating reusable components and
+          integrating api.
         </p>
         <p>
           The team in total consists of 3 developers. This is a passion project
@@ -64,20 +68,20 @@ const projects = [
     title: "WorshipHIM",
     imgSrc: "/project-imgs/wh_app.png",
     code: "https://www.github.com",
-    projectLink: "https://play.google.com/store/apps/details?id=com.ellinx.lightapps.worshiphim&hl=en&gl=US",
+    projectLink:
+      "https://play.google.com/store/apps/details?id=com.ellinx.lightapps.worshiphim&hl=en&gl=US",
     tech: ["React", "React Native", "Styled Components", "Android"],
     description:
       "WorshipHIM is a chord and lyrics app developed to help ease everyone in their worship to God.",
     modalContent: (
       <>
         <p>
-          WorshipHIM is a chord and lyrics app developed to help ease everyone in their worship to God.
-          So that all may know, from the rising of the sun to its setting,
-          That there is none besides God. He is the Lord and there&apos;s no other! Praise Him, Worship Him!
+          WorshipHIM is a chord and lyrics app developed to help ease everyone
+          in their worship to God. So that all may know, from the rising of the
+          sun to its setting, That there is none besides God. He is the Lord and
+          there&apos;s no other! Praise Him, Worship Him!
         </p>
-        <p>
-          This is a very fun project that i made using React Native.
-        </p>
+        <p>This is a very fun project that i made using React Native.</p>
       </>
     ),
   },
@@ -91,11 +95,13 @@ const projects = [
       "Landing Page of WorshipHIM mobile application for showcasing feature rich and tools of the app.",
     modalContent: (
       <>
-        <p>This is another self project that i made to showcase our mobile app.</p>
         <p>
-          I integrated ko-fi donation and uses Template for this Nextjs Project. 
-          I also use a plugin for scrapping data from our google play to display directly
-          on the landing page.
+          This is another self project that i made to showcase our mobile app.
+        </p>
+        <p>
+          I integrated ko-fi donation and uses Template for this Nextjs Project.
+          I also use a plugin for scrapping data from our google play to display
+          directly on the landing page.
         </p>
       </>
     ),
